ee0ca35a60643920d6f0e8bd504c08bb78b68f45
raw-mango-2003 <rakshitnagar123@gmail.com>
2026-09-17 19:27:25 +0530

    Modify CORS callback to handle disallowed origins
    Update CORS callback to return an error for disallowed origins.

--- server.ts
+++ server.ts
@@ -99,7 +99,7 @@ app.use(cors({
         }
       } catch {}
     }
-    return callback(null, true);
+    return callback(new Error('CORS origin not allowed'), false);
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
